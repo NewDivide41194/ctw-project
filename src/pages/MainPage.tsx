9ac8a7c18f9433f7d0854d64f50f884384c8b2d7
Hein Htet <heinhtet25944@gmail.com>
2023-02-03 16:45:22 +0630

    first upload

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import Pagination from "../components/pagination"
+
+const MainPage = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      <div>
+      <Pagination/>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
